e7c39e9f808f7cc8a157d1435306406a6e272dd0
VMhetar <mhetarvarad@gmail.com>
2026-01-05 11:40:17 +0530

    Updated belief_manager.py

---
+++ belief_manager.py
@@ -0,0 +1,11 @@
+from belief_math import compute_confidence
+from claim_types import DECAY_RATES
+
+def update_belief_confidence(belief):
+    decay_rate = DECAY_RATES[belief.claim_type]
+
+    belief.confidence = compute_confidence(
+        evidence_list=belief.evidence,
+        last_verified=belief.last_verified,
+        decay_rate=decay_rate
+    )
